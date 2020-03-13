548234333f00dd1663be0f87bc780004058259d2
Joe Shields <shields.john.joseph@gmail.com>
2020-03-12 22:13:42 -0500

    updated app.js

--- reactemployeedirectory/src/App.js
+++ reactemployeedirectory/src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import Table from './components/Table';
+import Main from './components/Main'
+import Api from "./utils/Api";
 
 import './App.css';
+
 
 function App() {
   return (
     <div>
-    <Header />
-    <SearchBar />
-    <Table />
+    <Header /> 
+    <Main />
     </div>
   );
 }
