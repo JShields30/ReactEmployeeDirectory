d419cf80e85dffcca709c51d9061180376902ac9
Paul <paul@cwik.us>
2020-03-14 19:36:25 -0500

    changes that got buggy on joes end so i am committing

--- reactemployeedirectory/src/components/Main/index.js
+++ reactemployeedirectory/src/components/Main/index.js
@@ -3,37 +3,56 @@ import Table from "..//Table/index"
 import Api from "../../utils/Api"
 
 class Main extends React.Component {
-    state = { 
+    state = {
         users: [],
         search: '',
-        filterUsers: [{}]
-        
-    }
+        filterUsers: []
+
+    };
+
     componentDidMount() {
         Api.getUsers().then(results => {
             this.setState({ users: results.data.results })
+            this.handleFilteredUsers();
         });
-    }
+    };
+
     handleFilteredUsers = () => {
-        
-        const newFilterUsers = this.state.users.filter(user =>{
-           return user.name.first.includes(this.state.search)  || user.name.last.includes(this.state.search)
-        })
+            console.log('newfilterusers triggerd')
+            let newFilterUsers;
+
+            if (this.state.search === '') {
+                console.log('nothing in search')
+                newFilterUsers = this.state.users
+
+            } else {
+                console.log(`search says: ${this.state.search}`)
+                newFilterUsers = this.state.users.filter(user => {
+                    return (user.name.first.toLowerCase().includes(this.state.search)  || user.name.last.toLowerCase().includes(this.state.search) )
+                })
+            }
+            
+        // console.log(newFilterUsers)
         this.setState({
             filterUsers: newFilterUsers
         })
-    }
+    };
+
     handleInputChange = e => {
         //do generic input change to handle state of search
         let value = e.target.value;
         const name = e.target.name;
         // Updating the input's state
         this.setState({
-          [name]: value
+            [name]: value
         });
-        handleFilteredUsers();
+        this.handleFilteredUsers();
     }
     render() {
+        // console.log(this.state.search)
+        console.log(this.state.users)
+        console.log(this.state.users[1])
+        console.log(this.state.filterUsers)
         return (
             <div>
                 <form
@@ -43,7 +62,7 @@ class Main extends React.Component {
                         className="form-control mr-sm-2"
                         type="text"
                         placeholder="Search"
-                        ariaLabel="Search"
+                        aria-label="Search"
                         name="search"
                         onChange={this.handleInputChange}
                     />
--- reactemployeedirectory/src/components/Table/index.js
+++ reactemployeedirectory/src/components/Table/index.js
@@ -1,40 +1,54 @@
 import React from "react";
 import SearchBar from "../SearchBar";
 
-function Table({users}) {
+function Table({ users }) {
   function dobDate(date) {
     const dobDate = new Date(date);
 
-return dobDate.toDateString();
+    return dobDate.toDateString();
   }
+  console.log(users)
   return (
     <>
-    {/* <SearchBar/> */}
+      {/* <SearchBar/> */}
 
-    <table className="table table-bordered table-dark">
-      <thead>
-        <tr>
-          <th scope="col">Image</th>
-          <th scope="col">Name (First and Last)</th>
-          <th scope="col">Phone Number</th>
-          <th scope="col">Email</th>
-          <th scope="col">DOB</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map(({ name, email, dob, phone, picture }) => {
-          return (
-            <tr>
-              <th scope="row"><img src={picture.medium} alt=" Not Loading" /></th>
-              <td>{name.first + " " + name.last}</td> 
-              <td>{phone}</td> 
-              <td>{email}</td> 
-              <td>{dobDate(dob.date)}</td> 
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+      <table className="table table-bordered table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col">Name (First and Last)</th>
+            <th scope="col">Phone Number</th>
+            <th scope="col">Email</th>
+            <th scope="col">DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.length ? (
+            users.map((user, i) => {
+              let { name, picture, phone, email, dob } = user;
+              let dobDate = new Date(dob.date)
+              let formatDob = dobDate.toString().split(' ');
+              let string = `${formatDob[1]} ${formatDob[2]}, ${formatDob[3]}`
+              console.log(formatDob)
+              console.log(dobDate)
+              console.log(dob.date)
+              console.log(`name: ${name} \n picture: ${picture} \n phone: ${phone}`)
+              console.log(user)
+              return (
+                <tr key={`${user}-${i}`}>
+                  <th scope="row"><img src={picture.medium} alt=" Not Loading" /></th>
+                  <td>{name.first + " " + name.last}</td> 
+                  <td>{phone}</td> 
+                  <td>{email}</td> 
+              <td>{string}</td> 
+                </tr>
+              );
+            })
+          ) : (
+              null
+            )}
+        </tbody>
+      </table>
     </>
   );
 }
